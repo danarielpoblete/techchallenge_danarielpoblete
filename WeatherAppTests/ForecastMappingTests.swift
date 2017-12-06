3263491e3e2c1e54235ce1f224f09ab56cd60541
Dan Poblete <dan.ariel.poblete@gmail.com>
2017-12-07 04:37:04 +1100

    Added final modifier

--- WeatherAppTests/ForecastMappingTests.swift
+++ WeatherAppTests/ForecastMappingTests.swift
@@ -10,7 +10,7 @@ import XCTest
 import ObjectMapper
 @testable import WeatherApp
 
-class ForecastMappingTests: XCTestCase {
+final class ForecastMappingTests: XCTestCase {
     
     private var forecast: Forecast!
     private var sampleDataJSONString: String!

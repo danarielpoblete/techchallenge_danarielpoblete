a1b1964e0f9b40237e771de25d93a2af9eedd29b
Dan Poblete <dan.ariel.poblete@gmail.com>
2017-12-07 02:55:03 +1100

    Created view model for forecast

--- WeatherApp/ViewModels/ForecastViewModel.swift
+++ WeatherApp/ViewModels/ForecastViewModel.swift
@@ -7,3 +7,100 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+enum ForecastViewModelFetchStatus {
+    case idle
+    case busy
+    case success
+    case failed
+}
+
+protocol ForecastViewModelProtocol {
+    var forecast: Driver<Forecast?> { get }
+    var fetchForecastStatus: Driver<ForecastViewModelFetchStatus> { get }
+    
+    func fetchForecast()
+}
+
+public final class ForecastViewModel: ForecastViewModelProtocol {
+
+    // MARK: - Properties
+    // Outputs
+    var forecast: Driver<Forecast?> {
+        return _forecast.asDriver(onErrorJustReturn: nil)
+    }
+    
+    var fetchForecastStatus: Driver<ForecastViewModelFetchStatus> {
+        return _fetchForecastStatus.asDriver()
+    }
+    
+    var action: Observable<Action> {
+        return _action.asObservable()
+    }
+    
+    // Helpers
+    private let _forecast = PublishSubject<Forecast?>()
+    private let _fetchForecastStatus = Variable<ForecastViewModelFetchStatus>(.idle)
+    private let _action = PublishSubject<Action>()
+    
+    private let disposeBag = DisposeBag()
+    
+    // Dependencies
+    private let weatherAPIService: WeatherAPIServiceProtocol
+    
+    // MARK: - Init
+    init(weatherAPIService: WeatherAPIServiceProtocol) {
+        self.weatherAPIService = weatherAPIService
+    
+        setupBindings()
+    }
+    
+    // MARK: - Public Methods
+    public func fetchForecast() {
+        _action.onNext(.fetchForecast)
+    }
+    
+    // MARK: - Private Methods
+    private func setupBindings() {
+        let latitude = 33.8650
+        let longitude = 151.2094
+        
+        action
+            .asObservable()
+            .throttle(2, latest: false, scheduler: MainScheduler.instance)
+            .observeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+            .filter {
+                $0 == .fetchForecast
+            }
+            .map { _ in
+                ()
+            }
+            .do(onNext: { [unowned self] _ in
+                self._fetchForecastStatus.value = .busy
+            })
+            .flatMap { [unowned self] _ in
+                self.weatherAPIService.fetchForecast(latitude: latitude, longitude: longitude)
+            }
+            .map {
+                Optional($0)
+            }
+            .do(
+                onNext: { [unowned self] _ in
+                    self._fetchForecastStatus.value = .success
+                    self._fetchForecastStatus.value = .idle
+                },
+                onError: { [unowned self] _ in
+                    self._fetchForecastStatus.value = .failed
+                    self._fetchForecastStatus.value = .idle
+            })
+            .asDriver(onErrorJustReturn: nil)
+            .drive(_forecast)
+            .disposed(by: disposeBag)
+    }
+    
+    enum Action {
+        case fetchForecast
+    }
+}

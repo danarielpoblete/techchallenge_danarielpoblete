91a459db277c686e9a6726410f4af0554bd33e2f
Dan Poblete <dan.ariel.poblete@gmail.com>
2017-12-08 06:01:12 +1100

    Added temperature high and low to ForecastData

--- WeatherApp/Models/ForecastData.swift
+++ WeatherApp/Models/ForecastData.swift
@@ -13,8 +13,11 @@ public struct ForecastData {
     let summary: String?
     let iconName: String?
     let temperature: Double?
+    let temperatureHigh: Double?
+    let temperatureLow: Double?
     let apparentTemperature: Double?
     let precipitationProbability: Double?
+    let time: Date
 }
 
 extension ForecastData: ImmutableMappable {
@@ -23,16 +26,22 @@ extension ForecastData: ImmutableMappable {
         summary = try? map.value("summary")
         iconName = try? map.value("icon")
         temperature = try? map.value("temperature")
+        temperatureHigh = try? map.value("temperatureHigh")
+        temperatureLow = try? map.value("temperatureLow")
         apparentTemperature = try? map.value("apparentTemperature")
         precipitationProbability = try? map.value("precipProbability")
+        time = try map.value("time", using: DateTransform())
     }
     
     public mutating func mapping(map: Map) {
         summary >>> map["summary"]
         iconName >>> map["icon"]
         temperature >>> map["temperature"]
+        temperatureHigh >>> map["temperatureHigh"]
+        temperatureLow >>> map["temperatureLow"]
         apparentTemperature >>> map["apparentTemperature"]
         precipitationProbability >>> map["precipProbability"]
+        time >>> (map["time"], DateTransform())
     }
 
 }

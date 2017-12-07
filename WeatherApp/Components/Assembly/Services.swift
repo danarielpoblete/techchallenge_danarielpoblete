85a804d01561abc67386d0a6555c35943a2c70ef
Dan Poblete <dan.ariel.poblete@gmail.com>
2017-12-08 06:02:24 +1100

    Created simple services struct

---
+++ WeatherApp/Components/Assembly/Services.swift
@@ -0,0 +1,26 @@
+//
+//  Services.swift
+//  WeatherApp
+//
+//  Created by Dan.Poblete on 8/12/17.
+//  Copyright © 2017 danarielpoblete. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+public struct Services {
+    
+    public let weatherAPIService: WeatherAPIServiceProtocol
+    
+    public init() {
+        let stubClosure: MoyaProvider<DarkSky>.StubClosure = { target in
+            switch target {
+            case .forecast: return .immediate
+            }
+        }
+        let provider = MoyaProvider<DarkSky>(stubClosure: stubClosure)
+        self.weatherAPIService = DarkSkyWeatherAPIService(provider: provider)
+    }
+    
+}

602e6faf255e6645ef99e20bc4c22237ec065850
Dan Poblete <dan.ariel.poblete@gmail.com>
2017-12-08 06:07:05 +1100

    Created ForecastDataViewController

---
+++ WeatherApp/ViewControllers/ForecastDataViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ForecastDataViewController.swift
+//  WeatherApp
+//
+//  Created by Dan.Poblete on 7/12/17.
+//  Copyright © 2017 danarielpoblete. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
+import PureLayout
+
+final class ForecastDataViewController: BaseViewController {
+    
+    enum Cell {
+        case current(forecastData: ForecastData?)
+        case daily(forecastData: ForecastData)
+    }
+    
+    // MARK: - Properties
+    // Views
+    public let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+    public let tableView = UITableView()
+    
+    // Helpers
+    private let disposeBag = DisposeBag()
+    
+    // Dependencies
+    private let viewModel: ForecastDataViewModelProtocol
+    
+    // MARK: - Init
+    init(viewModel: ForecastDataViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Overrides
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+    }
+    
+    // MARK: - Setup
+    private func setup() {
+        setupViews()
+        setupConstraints()
+        setupBindings()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .blue
+        
+        activityIndicator.hidesWhenStopped = true
+        
+        // Table View
+        tableView.register(CurrentForecastTableViewCell.self, forCellReuseIdentifier: "CurrentForecastTableViewCell")
+        tableView.register(DailyForecastTableViewCell.self, forCellReuseIdentifier: "DailyForecastTableViewCell")
+        tableView.backgroundColor = .clear
+        tableView.separatorColor = .clear
+        tableView.tableFooterView = UIView()
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 44
+    }
+    
+    private func setupConstraints() {
+        view.addSubview(tableView)
+        view.addSubview(activityIndicator)
+        
+        tableView.autoPinEdgesToSuperviewEdges()
+        activityIndicator.autoCenterInSuperview()
+    }
+    
+    private func setupBindings() {
+        viewWillAppearAnimated
+            .drive(onNext: { [unowned self] animated in
+                self.navigationController?.setNavigationBarHidden(false, animated: animated)
+            })
+            .disposed(by: disposeBag)
+    }
+}
